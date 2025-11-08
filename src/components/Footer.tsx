3ca4239177bf6f114ef574ce7a8be977c9eb1031
Ethan Shang <34875955+ethanjshang@users.noreply.github.com>
2025-11-07 22:07:16 -0800

    Update Footer.tsx
    Update disclaimer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,9 @@ const Footer = () => {
         <p className="text-center text-sm text-muted-foreground leading-relaxed">
           The personal finance information provided on this page is for educational purposes only. 
           The creators of this site do not offer personalized investment advice or services, nor do 
-          they recommend investors to buy or sell particular investments or securities.
+          they recommend investors to buy or sell particular investments or securities. The creators of this site
+          do not provide legal, tax, or accounting advice. You should consult your legal and/or tax advisors before
+          making any financial decisions. 
         </p>
       </div>
     </footer>
